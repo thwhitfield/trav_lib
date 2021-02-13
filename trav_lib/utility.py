a6058edc62a972bee06ade729d65b6f806af341c
Travis Whitfield <thwhitfield@gmail.com>
2021-02-13 00:58:37 -0500

    updated utility.py, updated copy_current_nb function to use pathlib, updated output_model function to output an experiment log.

--- trav_lib/utility.py
+++ trav_lib/utility.py
@@ -7,6 +7,7 @@ import json
 import os
 import ipykernel
 import shutil
+import pathlib
 
 import pickle
 import os
@@ -36,28 +37,41 @@ def notebook_path():
 def copy_current_nb(new_name):
     nb = notebook_path()
     if nb:
-        new_path = os.path.join(os.path.dirname(nb), new_name+'.ipynb')
-        shutil.copy2(nb, new_path)
+        shutil.copy2(nb, new_name)
     else:
         print("Current notebook path cannot be determined.")
     return
 
-def output_model(model,predictions,model_dir):
-    """Output serialized model, predictions, and notebook used to make model/predictions"""
+def output_model(model,predictions,model_dir,metric,notes = ''):
+    """Output serialized model, predictions, and notebook used to make model/predictions. Also
+    add record to the experiment tracker."""
+    
+    experiment_log = model_dir / 'experiment_log.csv'
+    cur_time = datetime.datetime.now()
+    
+    if not os.path.isfile(experiment_log):
+        with open(experiment_log,'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(['Datetime','Model Name','Metric Score', 'Notes'])
     
     for i in range(1,1000):
-        model_name = '{}model_{:03d}.pkl'.format(model_dir,i)
+        model_name = model_dir / 'model_{:03d}.pkl'.format(i)
         
         # Check if filename exists, if not write files
         if not os.path.isfile(model_name):
             with open(model_name,'wb') as f:
                 pickle.dump(model,f)
                 
-            prediction_name = '{}predictions_{:03d}.csv'.format(model_dir,i)
+            prediction_name = model_dir / 'predictions_{:03d}.csv'.format(i)
             predictions.to_csv(prediction_name,index=False)
             
-            notebook_name = '{}notebook_{:03d}'.format(model_dir,i)
+            notebook_name = model_dir / 'notebook_{:03d}.ipynb'.format(i)
             copy_current_nb(notebook_name)
+            
+            with open(experiment_log, 'a', newline='') as csvfile:
+                csvwriter = csv.writer(csvfile)
+                csvwriter.writerow([cur_time,f'model_{i:03d}',metric,notes])
+            
             return
     print('Error: More than 1000 models in folder')
     return
