46185f5545d958eba1538c769a98d07908dd0d19
Travis Whitfield <thwhitfield@gmail.com>
2021-10-06 17:26:42 -0500

    created snake_case function in data_prep.py

--- trav_lib/data_prep.py
+++ trav_lib/data_prep.py
@@ -1,9 +1,24 @@
 import pandas as pd
 import numpy as np
+import re
 
 def reduce_memory(df, cat_cols = None, verbose = True):
     """Reduce dtype size of dataframe columns to smallest available & convert specified columns
-    to categorical."""
+    to categorical.
+    
+    Parameters
+    ----------
+    df: pandas dataframe
+        dataframe which will be reduced in size
+    cat_cols: list (default: None)
+        list of column names to convert to categorical
+    verbose: bool (default: True)
+    
+    Returns
+    -------
+    df: pandas dataframe
+        dataframe after it has been reduced in size
+    """
     
     if verbose:
         memory_before = df.memory_usage(deep=True).sum() / 1e6
@@ -29,3 +44,23 @@ def reduce_memory(df, cat_cols = None, verbose = True):
         print(f'decreased by: {100*(memory_before - memory_after) / memory_before:.1f}%')
     
     return(df)
+
+def snake_case(word):
+    """Convert word to snake_case
+    
+    Parameters
+    ----------
+    word: string
+    
+    Returns
+    -------
+    word: string
+    """
+    
+    word = re.sub(r"([A-Z]+)([A-Z][a-z])", r'\1_\2', word)
+    word = re.sub(r"([a-z\d])([A-Z])", r'\1_\2', word)
+    word = word.replace("-", "_").replace(' ','_')
+    word = word.lower()
+    
+    return(word)
+                  
